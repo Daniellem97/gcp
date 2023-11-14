35160f55b4e1e8b37a2877624321ba1517460912
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-11-14 15:06:13 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "api_key" {
+variable "datadog_api_key" {
   description = "Datadog API Key"
   type        = string
 }
 
-variable "app_key" {
+variable "datadog_app_key" {
   description = "Datadog Application Key"
   type        = string
 }
