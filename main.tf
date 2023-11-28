601159aed0e2695b89bd511be6ce652ae89c9e86
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-11-28 21:45:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,72 +1,7 @@
-resource "google_storage_bucket" "static-site" {
-  name          = "image-store.com"
-  location      = "EU"
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
-}
-
-resource "datadog_monitor" "foo" {
-  name               = "Name for monitor foo"
-  type               = "metric alert"
-  message            = "Monitor triggered. Notify: @hipchat-channel"
-  escalation_message = "Escalation message @pagerduty"
-
-  query = "avg(last_1h):avg:aws.ec2.cpu{environment:foo,host:foo} by {host} > 4"
-
-  monitor_thresholds {
-    warning  = 2
-    critical = 4
-  }
-
-  include_tags = true
-
-  tags = ["foo:bar", "team:fooBar"]
-}
-
-resource "datadog_synthetics_test" "test_uptime" {
-  name      = "An Uptime test on example.org"
-  type      = "api"
-  subtype   = "http"
-  status    = "live"
-  message   = "Notify @pagerduty"
-  locations = ["aws:eu-central-1"]
-  tags      = ["foo:bar", "foo", "env:test"]
-
-  request_definition {
-    method = "GET"
-    url    = "https://www.example.org"
-  }
-
-  request_headers = {
-    Content-Type = "application/json"
-  }
-
-  assertion {
-    type     = "statusCode"
-    operator = "is"
-    target   = "200"
-  }
-
-  options_list {
-    tick_every = 900
-    retry {
-      count    = 2
-      interval = 300
-    }
-    monitor_options {
-      renotify_interval = 120
-    }
+{
+  "name": "my-unique-bucket-name",
+  "type": "storage.v1.bucket",
+  "properties": {
+    "location": "US"
   }
 }
