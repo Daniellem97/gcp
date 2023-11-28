4b78042170bfaaaeb448c9c8940b89d45ca98465
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-11-28 21:59:37 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("/mnt/workspace/spacelift.oidc")
+  credentials = file("/mnt/workspace/gcp.json")
   project     = "seraphic-ripple-403315"
   region      = "europe-west1"
 }
