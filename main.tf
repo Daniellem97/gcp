cfa0f1f9d1fa71cdac6efe5d730b45a8510788c7
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-12-29 21:53:06 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,19 +3,3 @@ resource "google_storage_bucket" "my_bucket" {
   location = "US"
 }
 
-resource "google_cloud_instance" "default" {
-  name             = "example-instance"
-  database_version = "MYSQL_5_7"
-
-  settings {
-    tier = "db-f1-micro"
-
-    ip_configuration {
-      authorized_networks {
-        name  = "all"
-        value = "0.0.0.0/0"
-      }
-    }
-  }
-}
-
