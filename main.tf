ffdf4da14fa5512729d518c7c799141c8e151700
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-12-30 14:35:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,17 @@ resource "google_storage_bucket" "my_bucket" {
   location = "US"
 }
 
+resource "google_project_iam_policy" "project" {
+  project     = "your-project-id"
+  policy_data = data.google_iam_policy.admin.policy_data
+}
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/editor"
+
+    members = [
+      "user:jane@example.com",
+    ]
+  }
+}
