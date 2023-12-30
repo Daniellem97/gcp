242d8a8d0cf9cbac4ecee9629c6e36387a0d966f
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-12-30 18:56:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,41 +3,20 @@ resource "google_storage_bucket" "my_bucket" {
   location = "US"
 }
 
-resource "google_project_iam_policy" "project" {
-  project     = "your-project-id"
-  policy_data = data.google_iam_policy.admin.policy_data
-}
+resource "datadog_monitor" "foo" {
+  name               = "Name for monitor foo"
+  type               = "metric alert"
+  message            = "Monitor triggered. Notify: @hipchat-channel"
+  escalation_message = "Escalation message @pagerduty"
 
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/editor"
+  query = "avg(last_1h):avg:aws.ec2.cpu{environment:foo,host:foo} by {host} > 4"
 
-    members = [
-      "user:jane@example.com",
-    ]
+  monitor_thresholds {
+    warning  = 2
+    critical = 4
   }
+
+  include_tags = true
+
+  tags = ["foo:bar", "team:fooBar"]
 }
-
-resource "google_sql_database_instance" "example" {
-  name             = "example-instance"
-  database_version = "MYSQL_5_7"
-  region           = "us-central1" # Change this to your desired region
-
-  settings {
-    tier = "db-f1-micro"
-
-    ip_configuration {
-      ipv4_enabled    = true
-      authorized_networks {
-        name    = "private-network"
-        value   = "0.0.0.0/0"  # Example CIDR block for your private network
-      }
-    }
-  }
-}
-
-resource "google_sql_database" "example" {
-  name     = "example-database"
-  instance = google_sql_database_instance.example.name
-}
-
