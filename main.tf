72d20feb7cb4eba10f9cc59b8f9dc079e9c5327f
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-12-30 23:46:28 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 resource "google_storage_bucket" "my_bucket" {
   name     = "plsplsplsplsplswork"
   location = "US"
+labels = {
+    app   = "your_app_value"
+    infra = "your_infra_value"
+  }
 }
 
 resource "datadog_monitor" "foo" {
