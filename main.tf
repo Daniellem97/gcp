78ba9a6c27df2177c571ac00c76efb3ba69fa597
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-12-30 15:04:57 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,16 @@ data "google_iam_policy" "admin" {
     ]
   }
 }
+
+resource "google_cloud_instance" "default" {
+  name             = "example-instance"
+  database_version = "MYSQL_5_7"
+
+	@@ -13,7 +13,7 @@ resource "google_sql_database_instance" "default" {
+    ip_configuration {
+      authorized_networks {
+        name  = "all"
+        value = "0.0.0.0/0"
+      }
+    }
+  }
